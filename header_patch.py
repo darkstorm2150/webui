1d3930e0950f6990d9de96d4d06017e68788790f
camenduru <54370274+camenduru@users.noreply.github.com>
2023-01-09 15:02:57 +0300

    Update header_patch.py

--- header_patch.py
+++ header_patch.py
@@ -3,8 +3,8 @@
                 gr.HTML(f'''
                 <div class="gr-prose" style="max-width: 80%">
                     <p>⚛ Automatic1111 Stable Diffusion Protogen x3.4 Web UI | Running model: darkstorm2150/ProtoGen_X3.4</p>
-                    <p>You can find more information on <a href="this model at civitai.com" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p> 
-                    <p> Code by <a href="camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a> </p>
+                    <p>You can find more information on <a href="https://civitai.com/models/3666/protogen-x34-photorealism-official-release" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p> 
+                    <p> Code by camenduru <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a></p>
                 </div>
                 ''')
             elif(is_spaces):
@@ -13,15 +13,15 @@
                     gr.HTML(f'''
                     <div class="gr-prose" style="max-width: 80%">
                         <p>⚛ Automatic1111 Stable Diffusion Protogen x3.4 Web UI | Running model: darkstorm2150/ProtoGen_X3.4</p>
-                        <p>You can find more information on <a href="this model at civitai.com" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p>  
-                        <p>Code by camenduru <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</p>
+                        <p>You can find more information on <a href="https://civitai.com/models/3666/protogen-x34-photorealism-official-release" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p>  
+                        <p>Code by camenduru <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a></p>
                     </div>
                 ''')
                 else:
                     gr.HTML(f'''
                     <div class="gr-prose" style="max-width: 80%">
                         <p>⚛ Automatic1111 Stable Diffusion Protogen x3.4 Web UI | Running model: darkstorm2150/ProtoGen_X3.4</p>
-                        <p>You can find more information on <a href="this model at civitai.com" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p>  
-                        <p>Code by camenduru <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</p>
+                        <p>You can find more information on <a href="https://civitai.com/models/3666/protogen-x34-photorealism-official-release" style="target=" _blank"="">https://civitai.com/models/3666/protogen-x34-photorealism-official-release</a></p>  
+                        <p>Code by camenduru <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a></p>
                     </div>
                 ''')
